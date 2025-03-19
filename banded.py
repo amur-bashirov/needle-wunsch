c4b257fb4486f04b3d4cb7d5b120f1edb01486f7
Amur Bashirov <blaccbaron@gmail.com>
2025-03-18 22:19:39 -0700

    completed logic for intiating matrix banded algorithm

--- banded.py
+++ banded.py
@@ -3,47 +3,58 @@ def banded(
         seq2: str,
         match_award=-3,
         indel_penalty=5,
-        banded_width=-1,
+        banded_width=7,
         sub_penalty=1,
         gap='-'
 ) -> tuple[float, str | None, str | None]:
-    """
-        Align seq1 against seq2 using Needleman-Wunsch
-        Put seq1 on left (j) and seq2 on top (i)
-        => matrix[i][j]
-        :param seq1: the first sequence to align; should be on the "left" of the matrix
-        :param seq2: the second sequence to align; should be on the "top" of the matrix
-        :param match_award: how many points to award a match
-        :param indel_penalty: how many points to award a gap in either sequence
-        :param sub_penalty: how many points to award a substitution
-        :param banded_width: banded_width * 2 + 1 is the width of the banded alignment; -1 indicates full alignment
-        :param gap: the character to use to represent gaps in the alignment strings
-        :return: alignment cost, alignment 1, alignment 2
-    """
-    col = len(seq2)
-    row = len(seq1)
-    arr = [[0] * (col + 2) for _ in range(row + 2)]
 
-    letters1 = list(seq2)
-    letters2 = list(seq1)
-
-    arr[0][1] = " "
-    arr[1][0] = " "
+    col = len(seq1)
+    arr = [[0]]
+    d = (banded_width - 1) // 2
 
 
-    for i in range(2, col + 2):
-        arr[0][i] = letters1[i - 2] + " "
+    alignment1 = list(seq1)
+    alignment2 = list(seq2)
 
-    for i in range(2, row + 2):
-        arr[i][0] = letters2[i - 2] + " "
+    for i in range(col + 1):
+        if i != 0:
+            arr.append([])
+        for j in range(d+i+1):
 
-    for i in range(row + 2):
-        string = "|"
-        for j in range(col + 2):
-            string += f" {arr[i][j]} |"
-        print(string)
+            if j < 1:
+                letter2 = alignment2[0]
+                delete = float('inf')
+                substitution = float('inf')
+            else:
+                letter2 = alignment2[j - 1]
+                delete = arr[i][j - 1] + indel_penalty
+            if i < 1:
+                letter1 = alignment1[0]
+                insert = float('inf')
+                substitution = float('inf')
+            else:
+                letter1 = alignment1[i - 1]
+                insert = arr[i - 1][j] + indel_penalty
+            if j >= 1 and i >= 1:
+                substitution = arr[i - 1][j - 1] + (match_award if letter1 == letter2 else sub_penalty)
+            if i == 0 and j == 0:
+                continue
 
 
+            if substitution <= delete and substitution <= insert:
+                arr[i].append(substitution)
+
+
+            elif delete <= insert:
+                arr[i].append(delete)
+
+            else:
+                arr[i].append(insert)
+
+
+    cost = arr[col][d]
+
+    return cost, alignment1, alignment2
 
 
 
@@ -108,6 +119,6 @@ def retrace(arr, alignment1, alignment2, col, row,
 
 
 if __name__ == "__main__":
-    seq1 = 'ATGCATGC'
-    seq2 = 'ATGGTGC'
+    seq1 = 'THARS'
+    seq2 = 'OTHER'
     banded(seq1, seq2)
